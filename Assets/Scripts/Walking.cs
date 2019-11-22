a9606a2af98a82f87f9aea949f31b5901584424d
NofarUliel <nofaruliel@gmail.com>
2019-11-22 21:11:53 +0200

    add vuforia
    add vuforia to project add build the game in ar

---
+++ Assets/Scripts/Walking.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walking : MonoBehaviour
+{
+    float speed = 20.0F;
+    float rotationSpeed = 90.0F;
+
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if (Input.GetKeyDown("right"))
+        {
+           
+        }
+
+
+    }
+}
