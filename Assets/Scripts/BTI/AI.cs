423fa77ae88ee4916157b4954fb935f46a5ad706
NofarUliel <nofaruliel@gmail.com>
2019-11-24 12:41:24 +0200

    add panda and build BT the player go to ball

---
+++ Assets/Scripts/BTI/AI.cs
@@ -0,0 +1,36 @@
+﻿using Panda;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    public Transform other_player;
+    public Transform ball;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    
+
+    [Task]
+    public void GoToBall()
+    {
+        Vector3 target = ball.transform.position;
+        agent.SetDestination(target);
+        if (Task.isInspected)
+            Task.current.debugInfo = string.Format("t={0:0.00}", Time.time);
+
+        if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+        {
+            Task.current.Succeed();
+        }
+
+    }
+   
+}
